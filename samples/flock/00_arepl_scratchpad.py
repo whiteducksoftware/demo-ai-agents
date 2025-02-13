4b99a767de7ecaaee2bcbda9f71119b16436fcc4
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-02-13 20:39:04 +0100

    scratchpad

--- samples/flock/00_arepl_scratchpad.py
+++ samples/flock/00_arepl_scratchpad.py
@@ -1,2 +1,24 @@
 import asyncio
 from dataclasses import dataclass
+
+from flock.core import Flock, FlockAgent
+
+
+@dataclass
+class MyCoolBlog:
+    funny_blog_title: str
+    blog_headers: list
+    fitting_emoji: str
+    extra_ordinary_ideas: list[str]
+    blog_summary: str
+
+
+flock = Flock(local_debug=True)
+
+bloggy = FlockAgent(name="Bloggy", input="blog_idea", output="mycoolblog: MyCoolBlog")
+
+flock.add_agent(bloggy)
+
+result = asyncio.run(
+    flock.run_async(start_agent=bloggy, input={"blog_idea": "Rosenheim"})
+)
